030c14aeef1e59f317c708c1037d1c84177e0fe1
Thien <phamngocthien.dev@gmail.com>
2024-07-14 09:29:23 +0700

    update-button

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import 'primeicons/primeicons.css';
 import { PrimeReactProvider } from 'primereact/api';
 import 'primeflex/primeflex.css';
 import 'primereact/resources/primereact.css';
+import { Button } from 'primereact/button';
 export default function RootLayout({ children }) {
   const [theme, setTheme] = useState('saga-blue');
   const changeTheme = (themePath) => {
@@ -19,9 +20,9 @@ export default function RootLayout({ children }) {
         <React.StrictMode>
           <PrimeReactProvider>
           <div>
-              <button onClick={() => changeTheme('saga-blue')}>Saga Blue</button>
-              <button onClick={() => changeTheme('lara-light-cyan')}>Lara Light Cyan</button>
-              <button onClick={() => changeTheme('lara-light-green')}>Lara Light Green</button>
+              <Button className='mx-3' onClick={() => changeTheme('saga-blue')}>Saga Blue</Button>
+              <Button className='mx-3' onClick={() => changeTheme('lara-light-cyan')}>Lara Light Cyan</Button>
+              <Button className='mx-3' onClick={() => changeTheme('lara-light-green')}>Lara Light Green</Button>
             </div>
               {children}
           </PrimeReactProvider>
